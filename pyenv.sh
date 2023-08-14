08eaf93743b4921959412b1a9505abbd277507a0
Makoto Oda <truth_jp_4133@yahoo.co.jp>
2023-08-14 13:40:23 +0900

    fix pyenv statement order

--- pyenv.sh
+++ pyenv.sh
@@ -35,9 +35,9 @@ sudo apt -y update; sudo apt -y install build-essential libssl-dev zlib1g-dev \
 libbz2-dev libreadline-dev libsqlite3-dev curl \
 libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
 
+. ~/.bash_profile
+
 pyenv install 3.11.4
 pyenv global 3.11.4
-
-. ~/.bash_profile
 
 exit 0
