f4d0c962ca5b065521a3795a32af360a9dd2c0e8
Makoto Oda <truth_jp_4133@yahoo.co.jp>
2023-09-30 13:53:11 +0900

    fix apt output

--- _git.sh
+++ _git.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-dpkg -l git && sudo apt -y purge git
-dpkg -l git-all || sudo apt -y install git-all
+dpkg -l git && sudo apt -qq purge git
+dpkg -l git-all || sudo apt -qq install git-all
 
 exit 0
