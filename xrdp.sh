0fb00cd15748d5886e231d9cad6030142f4a6f8c
Makoto Oda <truth_jp_4133@yahoo.co.jp>
2023-09-30 12:58:47 +0900

    allow colord, package management

--- xrdp.sh
+++ xrdp.sh
@@ -6,4 +6,26 @@ if [ $? -ne 0 ]; then
     sudo apt -y -q install xfce4 xfce4-goodies fonts-noto fcitx5-mozc xrdp
 fi
 
+# Configure the policy xrdp session
+sudo tee /etc/polkit-1/localauthority/50-local.d/45-allow-colord.pkla <<EOF
+[Allow Colord all Users]
+Identity=unix-user:*
+Action=org.freedesktop.color-manager.create-device;org.freedesktop.color-manager.create-profile;org.freedesktop.color-manager.delete-device;org.freedesktop.color-manager.delete-profile;org.freedesktop.color-manager.modify-device;org.freedesktop.color-manager.modify-profile
+ResultAny=no
+ResultInactive=no
+ResultActive=yes
+EOF
+
+# https://askubuntu.com/questions/1193810/authentication-required-to-refresh-system-repositories-in-ubuntu-19-10
+sudo tee /etc/polkit-1/localauthority/50-local.d/46-allow-update-repo.pkla<<EOF
+[Allow Package Management all Users]
+Identity=unix-user:*
+Action=org.freedesktop.packagekit.system-sources-refresh
+ResultAny=yes
+ResultInactive=yes
+ResultActive=yes
+EOF
+
+im-config -n fcitx5
+
 exit 0
