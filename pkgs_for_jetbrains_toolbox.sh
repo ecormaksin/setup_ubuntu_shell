2ff5d91ea9b242ea56ab1c2e70f1efcb542a5b24
Makoto Oda <truth_jp_4133@yahoo.co.jp>
2023-10-22 00:35:50 +0900

    add shell for jetbrains toolbox

---
+++ pkgs_for_jetbrains_toolbox.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+sudo apt-get -y install libfuse2 libxi6 libxrender1 libxtst6 mesa-utils libfontconfig libgtk-3-bin tar
+
+# sudo tar xzf jetbrains-toolbox-*.tar.gz --strip-components 1 -C /usr/local/bin
