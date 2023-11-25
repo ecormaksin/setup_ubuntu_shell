744ef40fe9f953ecd332a6a31ca817bb9fabc0f9
Makoto Oda <truth_jp_4133@yahoo.co.jp>
2023-11-25 13:49:26 +0900

    append direnv

--- nix.sh
+++ nix.sh
@@ -1,8 +1,20 @@
 #!/usr/bin/env bash
 
 which nix >/dev/null
-if [ $? -eq 0 ]; then
-    exit 0
+if [ $? -ne 0 ]; then
+    curl -L https://nixos.org/nix/install | sh -s -- --daemon
 fi
 
-curl -L https://nixos.org/nix/install | sh -s -- --daemon
+dpkg -l | grep -E "^ii( )+direnv" >/dev/null
+if [ $? -ne 0 ]; then
+    sudo apt-get -y update
+    sudo apt-get -y install direnv
+fi
+
+cat ~/.bashrc | grep "direnv hook bash" >/dev/null
+[ $? -ne 0 ] && tee -a ~/.bashrc <<"EOF" >/dev/null
+
+eval "$(direnv hook bash)"
+EOF
+
+export DIRENV_LOG_FORMAT=
