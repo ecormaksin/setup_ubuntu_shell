cc12ba70fd89c92b984f2f0b78f7cdca687d55c2
Makoto Oda <truth_jp_4133@yahoo.co.jp>
2024-05-01 00:05:45 +0900

    voltaコマンドの認識について.bashrcの再読み込みではダメだった

--- volta.sh
+++ volta.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
 
-SHELL_RC_FILE_PATH=$HOME/.bashrc
-
 which volta >/dev/null
 if [ $? -ne 0 ]; then
     curl https://get.volta.sh | bash
 fi
 
-. "${SHELL_RC_FILE_PATH}"
+exec $SHELL -l
 
 which node >/dev/null
 if [ $? -ne 0 ]; then
