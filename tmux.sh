4d642c24750d7f4fd1fc7adfd607130701f4e223
Makoto Oda <truth_jp_4133@yahoo.co.jp>
2025-03-20 12:06:52 +0900

    fix tmux shell

--- tmux.sh
+++ tmux.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-dpkg -l | grep -E "^ii( )+vim" >/dev/null
+dpkg -l | grep -E "^ii( )+tmux" >/dev/null
 if [ $? -ne 0 ]; then
     sudo apt-get -y update
-    sudo apt-get -y install vim
+    sudo apt-get -y install tmux
 fi
 
 curl --output ~/.tmux.conf --silent https://raw.githubusercontent.com/ecormaksin/tmux_conf/main/.tmux.conf
