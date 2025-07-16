f3d4c7a5e979cb062500f0bdb52d22fe76979ba3
Makoto Oda <truth_jp_4133@yahoo.co.jp>
2025-07-16 20:51:52 +0900

    ghq multi root

--- ghq-peco-alias.sh
+++ ghq-peco-alias.sh
@@ -4,11 +4,11 @@ BASH_ALIASES_FILE_PATH=$HOME/.bash_aliases
 
 [ ! -e "${BASH_ALIASES_FILE_PATH}" ] && touch "${BASH_ALIASES_FILE_PATH}"
 
-cat "${BASH_ALIASES_FILE_PATH}" | grep -E "^alias gp='cd \\$\(ghq root\)/\\$\(ghq list \| peco\)'$" >/dev/null
+cat "${BASH_ALIASES_FILE_PATH}" | grep -E "^alias gp='cd \\$\(ghq list --full-path \| peco\)'$" >/dev/null
 if [ $? -eq 0 ]; then
     exit 0
 fi
 
 echo '' >> "${BASH_ALIASES_FILE_PATH}"
-echo 'alias gp='\''cd $(ghq root)/$(ghq list | peco)'\' >> "${BASH_ALIASES_FILE_PATH}"
+echo 'alias gp='\''cd $(ghq list --full-path | peco)'\' >> "${BASH_ALIASES_FILE_PATH}"
 . "${BASH_ALIASES_FILE_PATH}"
