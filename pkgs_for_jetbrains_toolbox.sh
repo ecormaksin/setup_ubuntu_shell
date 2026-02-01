e6d95748be876c8a4f0d9bddc7f849ba3caf8024
Makoto Oda <truth_jp_4133@yahoo.co.jp>
2026-02-01 13:50:53 +0900

    jetbrains-toolbox change extract directory

--- pkgs_for_jetbrains_toolbox.sh
+++ pkgs_for_jetbrains_toolbox.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-sudo apt-get -y install libfuse2 libxi6 libxrender1 libxtst6 mesa-utils libfontconfig libgtk-3-bin tar
+sudo apt-get -y install libfuse2 libxi6 libxrender1 libxtst6 mesa-utils libfontconfig libgtk-3-bin tar dbus-user-session
 
-# sudo tar xzf jetbrains-toolbox-*.tar.gz --strip-components 1 -C /usr/local/bin
+# sudo tar xzf jetbrains-toolbox-*.tar.gz --strip-components 1 -C <installation_directory>
