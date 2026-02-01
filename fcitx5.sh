7f660321134a43d1c45d5132bcbb2f9c51ffbdc5
Makoto Oda <truth_jp_4133@yahoo.co.jp>
2026-02-01 13:04:11 +0900

    add fcitx5 config, fontend, im-config

--- fcitx5.sh
+++ fcitx5.sh
@@ -2,7 +2,7 @@
 
 dpkg -l | grep -E "^ii( )+fcitx5" >/dev/null
 if [ $? -ne 0 ]; then
-    sudo apt-get -y install zenity fcitx5 fcitx5-mozc
+    sudo apt-get -y install zenity fcitx5 fcitx5-mozc fcitx5-config-qt fcitx5-frontend-all fcitx5-frontend-fbterm fcitx5-frontend-gtk2 fcitx5-frontend-gtk3 fcitx5-frontend-gtk4 fcitx5-frontend-qt5 fcitx5-frontend-qt6 fcitx5-frontend-tmux im-config
 fi
 
 im-config -n fcitx5
